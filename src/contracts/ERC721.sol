f121f68809181887391a2f6d1a9b00ccf30b4f8f
DavitMkhitaryan <davitmkhitaryanit@gmail.com>
2022-06-10 16:40:09 +0400

    ERC721 Connector and Metadata added

---
+++ src/contracts/ERC721.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+/*
+    Buliding out the minting functionality:
+        a. nft to point to an address
+        b. keep track of the token ids
+        c. keep track of the token owner addresses to token ids
+        d. keep track of how many tokens an owner address has
+        e. create an event that emits a transfer log - contract address where it is being minted to, the id
+*/
+contract ERC721 {
+
+}
