014ec127c7a4ce68a6d9114af2b1fcb53456de42
DavitMkhitaryan <davitmkhitaryanit@gmail.com>
2022-06-30 09:48:45 +0400

    Added basic minting functionality

--- src/contracts/ERC721.sol
+++ src/contracts/ERC721.sol
@@ -12,4 +12,30 @@ pragma solidity ^0.8.0;
 */
 contract ERC721 {
 
+    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
+
+
+    // Mapping from token id to owner
+    mapping(uint256 => address) private _tokenOwner;
+
+    // Mapping from owner to number of owned tokens
+    mapping(address => uint256) private _ownedTokensCount;
+
+    function _exists(uint256 tokenId) internal view returns(bool) {
+        address owner = _tokenOwner[tokenId];
+        return owner != address(0);
+    }
+
+    function _mint(address to, uint256 tokenId) internal {
+        require(to != address(0), 'ERC721: Minting to the zero address');
+        require(!_exists(tokenId), 'ERC721: Token already minted');
+        _tokenOwner[tokenId] = to;
+        _ownedTokensCount[to] += 1;
+
+        // address(0) is default value for now, but it should be the contract address
+        emit Transfer(address(0), to, tokenId);
+    }
+
+
+
 }
--- src/contracts/KryptoBirdz.sol
+++ src/contracts/KryptoBirdz.sol
@@ -5,6 +5,23 @@ import './ERC721Connector.sol';
 
 contract Kryptobird is ERC721Connector {
 
+    // Array to store our NFTs
+    string[] public kryptoBirdz;
+
+    mapping(string => bool) _kryptoBirdzExists;
+
+    function mint(string memory _kryptoBird) public {
+
+        require(!_kryptoBirdzExists[_kryptoBird], 'Error: kryptoBird already exists');
+        
+        kryptoBirdz.push(_kryptoBird);
+        uint256 _id = kryptoBirdz.length -1;
+
+        _mint(msg.sender, _id);
+
+        _kryptoBirdzExists[_kryptoBird] = true;
+    }
+
     constructor() ERC721Connector("Kryptobird", "KBIRDZ") {
 
     }
