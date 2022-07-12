358c94a19fc20d37e9c23a01e0610b86978e4a29
DavitMkhitaryan <davitmkhitaryanit@gmail.com>
2022-07-12 12:35:37 +0400

    Added balanceOf and ownerOf functions

--- src/contracts/ERC721.sol
+++ src/contracts/ERC721.sol
@@ -36,6 +36,17 @@ contract ERC721 {
         emit Transfer(address(0), to, tokenId);
     }
 
+    function balanceOf(address _owner) public view returns(uint256) {
+        require(_owner != address(0), 'ERC721: Owner input address is zero address');
+        return _ownedTokensCount[_owner];
+    }
+
+    function ownerOf(uint256 _tokenId) public view returns(address) {
+        address owner = _tokenOwner[_tokenId];
+        require(owner != address(0), 'ERC721: Owner input address is zero address');
+        return owner;
+    }
+
 
 
 }
