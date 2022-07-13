0dddd445228341fcf0fda1d1ab89ba3578f92dc3
DavitMkhitaryan <davitmkhitaryanit@gmail.com>
2022-07-13 15:26:10 +0400

    Added transfer from functionality

--- src/contracts/ERC721.sol
+++ src/contracts/ERC721.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-
 /*
     Buliding out the minting functionality:
         a. nft to point to an address
@@ -10,6 +9,7 @@ pragma solidity ^0.8.0;
         d. keep track of how many tokens an owner address has
         e. create an event that emits a transfer log - contract address where it is being minted to, the id
 */
+
 contract ERC721 {
 
     event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
@@ -20,6 +20,9 @@ contract ERC721 {
 
     // Mapping from owner to number of owned tokens
     mapping(address => uint256) private _ownedTokensCount;
+
+    // Mapping from token id to approved addresses
+    mapping(uint256 => address) private _tokenApprovals;
 
     function _exists(uint256 tokenId) internal view returns(bool) {
         address owner = _tokenOwner[tokenId];
@@ -47,6 +50,20 @@ contract ERC721 {
         return owner;
     }
 
+    function _transferFrom(address _from, address _to, uint256 _tokenId) internal {
+        require(_to != address(0), 'Transferring to zero address');
+        require(ownerOf(_tokenId) == _from, 'Trasnferring from not the owner of the token');
+        
+        _ownedTokensCount[_from] -= 1;
+        _ownedTokensCount[_to] += 1;
 
+        _tokenOwner[_tokenId] = _to;
+
+        emit Transfer(_from, _to, _tokenId);
+    }
+
+    function transferFrom(address _from, address _to, uint256 _tokenId) public {
+        _transferFrom(_from, _to, _tokenId);
+    }
 
 }
